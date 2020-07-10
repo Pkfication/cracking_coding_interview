0c80a528f67b2c55e2675134e976db8f6d9dd9d9
Pkfication <pankajgupta221b@gmail.com>
2020-07-11 00:35:44 +0530

    Create 1.6_str_compression.py

---
+++ 1.6_str_compression.py
@@ -0,0 +1,15 @@
+def str_compression(s1):
+    if len(s1) < 3: return s1
+
+    s = ""
+    letter = s1[0]
+    count = 1
+    for i in range(1, len(s1)):
+        if s1[i] == letter:
+            count += 1
+        else:
+            s += letter + str(count)
+            letter = s1[i]
+            count = 1
+    s += letter + str(count)
+    return s1 if len(s) > len(s1) else s
