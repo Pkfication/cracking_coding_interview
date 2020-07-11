f0efa7843d66042676d8ce63886a1b9238857fd4
Pkfication <pankajgupta221b@gmail.com>
2020-07-11 16:18:36 +0530

    Create 1.8_zero_matrix.py

---
+++ 1.8_zero_matrix.py
@@ -0,0 +1,48 @@
+def set_zero(arr):
+    rlen = len(arr)
+    clen = len(arr[0])
+    rows = [False] * rlen
+    columns = [False] * clen
+
+    for i in range(rlen):
+        for j in range(clen):
+            if arr[i][j] == 0:
+                rows[i] = True
+                columns[j] = True
+
+    for i in range(rlen):
+        if rows[i] == True:
+            nullify_row(arr, i)
+
+    for i in range(clen):
+        if columns[i] == True:
+            nullify_column(arr, i)
+
+def nullify_column(arr, column):
+    rlen = len(arr)
+    for i in range(rlen):
+        arr[i][column] = 0
+
+def nullify_row(arr, row):
+    clen = len(arr[0])
+    for i in range(clen):
+        arr[row][i] = 0
+
+
+def log(arr):
+    clen = len(arr[0])
+    rlen = len(arr)
+    for i in range(rlen):
+        for j in range(clen):
+            print str(arr[i][j]),
+        print
+arr = [
+    [1,0,3,4],
+    [5,6,7,8],
+    [9,10,0,12]
+]
+
+log(arr)
+print
+set_zero(arr)
+log(arr)
