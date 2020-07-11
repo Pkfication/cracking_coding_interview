6d92bc384779e5a161d0a251987958b9773d864e
Pkfication <pankajgupta221b@gmail.com>
2020-07-11 13:47:37 +0530

    Create 1.7_rotate_matrix.py

---
+++ 1.7_rotate_matrix.py
@@ -0,0 +1,42 @@
+def rotate_matrix(a):
+    log(arr)
+    transpose(arr)
+    reverse(arr)
+    print
+    log(arr)
+
+def log(arr):
+    R = C = len(arr)
+    for i in range(R):
+        for j in range(C):
+            print str(arr[i][j]) + " ",
+        print
+
+def transpose(arr):
+    R = C = len(arr)
+    for i in range(R):
+        for j in range(i, C):
+            t = arr[i][j]
+            arr[i][j] = arr[j][i]
+            arr[j][i] = t
+
+    return arr
+
+def reverse(arr):
+    C = len(arr)
+    for i in range(C):
+        j,k = 0, C-1
+        while j < k:
+            t = arr[j][i]
+            arr[j][i] = arr[k][i]
+            arr[k][i] = t
+            j += 1
+            k -= 1
+
+arr = [[1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12],
+        [13, 14, 15, 16]
+    ];
+
+rotate_matrix(arr)
